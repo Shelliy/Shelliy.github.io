61586347f7c96fca49aae2a184902b914d00b0b6
xueYe01 <1710@qq.com>
2023-11-30 22:01:38 +0800

    Site updated: 2023-11-30 22:01:37

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
     /** 计时起始时间，自行修改 **/
-    var start = new Date("2023/11/09 23:00:00");
+    var start = new Date("2023/11/30 21:00:00");
   
     function update() {
       var now = new Date();
